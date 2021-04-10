5a4c94a824409326a311d567805728a847ca9039
Tammy <tammycwong@gmail.com>
2021-04-10 18:32:15 -0400

    this is confusing

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,10 +3,40 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const foodList = foods.map((food)=> {
+    <li key={food.id} onClick={()=> handleClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+    </li>
+  });
+  return (
+    <div>
+      <button onClick={handleAddFood}>Add New Food</button>
+      <ul>{foodList}</ul>
+    </div>
+  );
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFOodArray = [...foods, newFood];
+    setFoods(newFoodArray);
+    //console.log(newFood);
+  }
+  function handleLiClick(id) {
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
+  }
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel : food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
   }
 
   return (
